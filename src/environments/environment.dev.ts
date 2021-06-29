f49366b3df5dae1343fffe4cb3e858512c68dbc6
Osvaldir Dias da Silva Junior <diasosvaldirjr@gmail.com>
2021-06-29 11:37:13 -0300

    fixing validate campain

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  baseUrl: 'https://dev.mobo.com.br/api/v1',
-  absoluteUrl: 'https://dev.mobo.com.br',
+  baseUrl: 'http://dev.mobo.com.br/api/v1',
+  absoluteUrl: 'http://dev.mobo.com.br',
   awsBase: 'https://dev-dragon-dowing-images.s3-sa-east-1.amazonaws.com',
   awsBaseFiles: 'https://dev-dragon-dowing-images.s3-sa-east-1.amazonaws.com/files',
 }

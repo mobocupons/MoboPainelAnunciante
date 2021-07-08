92e63db627681a84c5bb67ea36c03bbf6e31cde3
Osvaldir Dias da Silva Junior <diasosvaldirjr@gmail.com>
2021-07-08 12:29:56 -0300

    fixing environment

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,7 +1,17 @@
 export const environment = {
-  production: true,
-  baseUrl: 'http://dev.mobo.com.br/api/v1',
-  absoluteUrl: 'http://dev.mobo.com.br',
+  production: false,
+  baseUrl: 'https://dev.mobo.com.br/api/v1',
+  absoluteUrl: 'https://dev.mobo.com.br',
   awsBase: 'https://dev-dragon-dowing-images.s3-sa-east-1.amazonaws.com',
   awsBaseFiles: 'https://dev-dragon-dowing-images.s3-sa-east-1.amazonaws.com/files',
+  firebase:{
+    apiKey: "AIzaSyDnpz10tFxZhhXKwz-BiUVnaXYTzkArsmU",
+    authDomain: "mobo-9c945.firebaseapp.com",
+    databaseURL: "https://mobo-9c945.firebaseio.com",
+    projectId: "mobo-9c945",
+    storageBucket: "mobo-9c945.appspot.com",
+    messagingSenderId: "453237689250",
+    appId: "1:453237689250:web:436093176789ecda2000fb",
+    measurementId: "G-HQ83YF2K3Q"
 }
+};
